83ef4edcbaf81e6d61cfd4308c5e35e0f080e97b
jayden.williams <jayden.williams@CLC-LAB209-17.TCSD.tooelesd.org>
2018-09-04 13:18:43 -0600

    first commit

---
+++ src/jwHelloWorld/Hello_world.java
@@ -0,0 +1,23 @@
+/**
+ * hello world program print hello world in console
+ */
+package jwHelloWorld;
+
+/**
+ * @author jayden.williams
+ *9/18
+ *hello world
+ */
+public class Hello_world {
+	
+	
+		
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+	}
+
+}
